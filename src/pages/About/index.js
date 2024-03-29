b8384fcd8199fbb92e45c7a69f4f0740f2d08090
KhandekarPrateek <prateekkhandekar01@gmail.com>
2024-03-29 15:08:51 +0530

    CV download button is implemented

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,9 +1,51 @@
 import React from "react";
+import { Button, Col, Row } from "reactstrap";
+const PDF_FILE_URL = `${window.location.origin}/portfolio.pdf`;
 
 const About = () => {
+  const handleDownloadCv = (url) => {
+    const aTag = document.createElement("a");
+    aTag.href = url;
+    aTag.setAttribute("download", "PORTFOLIO.pdf");
+    document.body.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
+  };
   return (
     <div className="about-container " id="About">
-      About
+      <Row className="h-100 ">
+        <Col
+          sm={4}
+          className="d-flex justify-content-center align-items-center"
+        >
+          <div>
+            <h6 className="about-text">--Prateek Khandekar</h6>
+            <h1>React developer based in Indore,MP</h1>
+            <p className="about-text">
+              Welcome to my website! I'm a Computer Science Engineering student
+              hailing from Indore, Madhya Pradesh. With a keen interest in web
+              development, I've delved into the realms of React and JavaScript,
+              honing my skills along the way. Proficient in problem-solving, I
+              thrive on challenges that push me to innovate and create. Explore
+              further to learn more about my journey and projects
+            </p>
+            <Button
+              onClick={() => {
+                handleDownloadCv(PDF_FILE_URL);
+              }}
+            >
+              Download Cv
+            </Button>
+          </div>
+        </Col>
+
+        <Col
+          sm={8}
+          className="d-flex justify-content-center align-items-center"
+        >
+          <div>info table</div>
+        </Col>
+      </Row>
     </div>
   );
 };
