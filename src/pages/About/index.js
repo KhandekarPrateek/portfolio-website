7876cfc55455b2fba74ea444b0d83bab736b0154
KhandekarPrateek <prateekkhandekar01@gmail.com>
2024-03-31 11:04:23 +0530

    frontend changes to about section

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -13,41 +13,43 @@ const About = () => {
     aTag.remove();
   };
   return (
-    <div className="about-container " id="About">
-      <Row>
-        <Col
-          sm={4}
-          className="d-flex justify-content-center align-items-center h-100"
-        >
-          <div>
-            <h6 className="about-text">--Prateek Khandekar</h6>
-            <h1>React developer based in Indore,MP</h1>
-            <p className="about-text">
-              Welcome to my website! I'm a Computer Science Engineering student
-              hailing from Indore, Madhya Pradesh. With a keen interest in web
-              development, I've delved into the realms of React and JavaScript,
-              honing my skills along the way. Proficient in problem-solving, I
-              thrive on challenges that push me to innovate and create. Explore
-              further to learn more about my journey and projects
-            </p>
-            <Button
-              onClick={() => {
-                handleDownloadCv(PDF_FILE_URL);
-              }}
-            >
-              Download Cv
-            </Button>
+    <Row className="about-container " id="About">
+      <Col
+        sm={6}
+        className="d-flex justify-content-center align-items-center h-100 p-5"
+      >
+        <div>
+          <div className="d-flex">
+            <div class="divider-1 me-3">
+              {" "}
+              <span></span>
+            </div>
+            <h3 class="heading-1">PRATEEK KHANDEKAR</h3>
           </div>
-        </Col>
+          <h1>React developer based in Indore,MP</h1>
+          <p className="about-text">
+            Welcome to my website! I'm a Computer Science Engineering student
+            hailing from Indore, Madhya Pradesh. With a keen interest in web
+            development, I've delved into the realms of React and JavaScript,
+            honing my skills along the way. Proficient in problem-solving, I
+            thrive on challenges that push me to innovate and create. Explore
+            further to learn more about my journey and projects
+          </p>
+          <Button
+            onClick={() => {
+              handleDownloadCv(PDF_FILE_URL);
+            }}
+            size="lg"
+          >
+            Download Cv
+          </Button>
+        </div>
+      </Col>
 
-        <Col
-          sm={8}
-          className="d-flex justify-content-center align-items-center "
-        >
-          <InfoTable />
-        </Col>
-      </Row>
-    </div>
+      <Col sm={6} className="d-flex justify-content-center align-items-center ">
+        <InfoTable />
+      </Col>
+    </Row>
   );
 };
 
