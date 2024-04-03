feb5f6af4f40dbce40da9bc00e3ebd5d0ae4356b
KhandekarPrateek <prateekkhandekar01@gmail.com>
2024-04-03 17:07:34 +0530

    home image responsive

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,26 +1,30 @@
 import React from "react";
 import { Col, Row } from "reactstrap";
 import pic from "../../pic.jpeg";
+
 const Home = () => {
   return (
-    <div className="home-container " id="Home">
-      <Row className="h-100 d-flex align-items-center justify-content-center">
-        <Col sm={5}>
-          <img
-            src={pic}
-            alt=""
-            className="rounded-circle d-flex align-items-center justify-content-center image-home p-5  "
-          />
+    <div
+      className="home-container d-flex align-items-center justify-content-center"
+      id="Home"
+    >
+      <Row>
+        <Col
+          sm={12}
+          md={5}
+          className="d-flex justify-content-center align-items-center"
+        >
+          <img src={pic} alt="" className="rounded-circle image-home p-3" />
         </Col>
 
         <Col
-          sm={7}
+          sm={12}
+          md={7}
           className="d-flex justify-content-center align-items-center"
         >
           <div>
             <h1 className="home-name">PRATEEK</h1>
-
-            <h1 className="home-surname"> KHANDEKAR</h1>
+            <h1 className="home-surname">KHANDEKAR</h1>
           </div>
         </Col>
       </Row>
